c4a2dcc6a2c04d256f1f166599420a1da9b1a02d
JikunLiu101 <1015482278@qq.com>
2025-04-26 18:04:21 +0800

    Disable eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config(
+    {
+      "extends": ["next/core-web-vitals"],
+      "rules": {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn"
+    }
+    }
+  ).extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
